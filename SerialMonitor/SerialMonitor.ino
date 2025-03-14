8d7b12ab79a15331761b4c99f1eb27304224d531
juvncent <junevincentmagsayofernandez@gmail.com>
2025-03-14 20:01:07 +0800

    added serialmonitor.ino lesson 8

---
+++ SerialMonitor/SerialMonitor.ino
@@ -0,0 +1,67 @@
+String input;
+const int LED1 = 2; 
+const int LED2 = 3; 
+const int LED3 = 4; 
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW); 
+  digitalWrite(LED3, LOW);
+
+  Serial.println("Please input any color of LED:");
+}
+
+void loop() {
+  while(Serial.available()) {
+    input = Serial.readString();// read the incoming data as string
+
+    input.toLowerCase();
+    if(input == "green"){
+      if(digitalRead(LED3)){
+        Serial.println("Green is turned off");
+        digitalWrite(LED3, LOW);
+      } else {
+        Serial.println("Green is turned on");
+        digitalWrite(LED3, HIGH);
+      }
+    }
+    
+    if (input == "blue"){
+      if(digitalRead(LED2)){
+        digitalWrite(LED2, LOW);
+        Serial.println("Green is turned off");
+      } else {
+        digitalWrite(LED2, HIGH);
+        Serial.println("Green is turned on");
+      }
+    }
+    
+    if (input == "yellow"){
+      if(digitalRead(LED1)){
+        digitalWrite(LED1, LOW);
+        Serial.println("Green is turned off");
+      } else {
+        digitalWrite(LED1, HIGH);
+        Serial.println("Green is turned on");
+      }
+    }
+
+    if(input == "off"){
+      digitalWrite(LED1, LOW);
+      digitalWrite(LED2, LOW); 
+      digitalWrite(LED3, LOW);
+    } else if (input == "on"){
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, HIGH); 
+      digitalWrite(LED3, HIGH);
+    }
+
+    Serial.println("Please input any color of LED:");
+    delay(500);
+  }
+}
